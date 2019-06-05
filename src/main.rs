e75b1c1d85853f00e40e5e2caddec1d25a2f3f3e
Nick Pfundstein <nickpfundstein@gmail.com>
2019-06-04 22:05:06 -0400

    run rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
+use base64::decode as base64_decode;
+use base64::encode as base64_encode;
+use bit_vec::BitVec;
 use hex::decode;
 use hex::encode as hex_encode;
-use base64::encode as base64_encode;
-use base64::decode as base64_decode;
 use std::cmp::Ordering;
 use std::fs::File;
-use std::io::BufReader;
 use std::io::BufRead;
+use std::io::BufReader;
 use std::io::Read;
-use bit_vec::BitVec;
 
 fn main() {
     let input = BufReader::new(File::open("6.txt").expect("File not found!"));
-    let base64 = input.lines()
+    let base64 = input
+        .lines()
         .map(|x| x.unwrap())
         .collect::<Vec<String>>()
         .join("");
@@ -23,26 +24,26 @@ fn main() {
     println!("{}", String::from_utf8(final_bytes).unwrap());
 }
 
-fn fixed_xor(input: &[u8], mask: &[u8]) -> Vec<u8>
-{
-    return input.into_iter()
+fn fixed_xor(input: &[u8], mask: &[u8]) -> Vec<u8> {
+    return input
+        .into_iter()
         .zip(mask.into_iter())
         .map(|(x, y)| x ^ y)
         .collect::<Vec<u8>>();
 }
 
-fn repeating_xor(input: &Vec<u8>, key: &Vec<u8>) -> Vec<u8>
-{
-    return input.into_iter()
+fn repeating_xor(input: &Vec<u8>, key: &Vec<u8>) -> Vec<u8> {
+    return input
+        .into_iter()
         .zip(key.iter().cycle().into_iter())
         .map(|(x, y)| x ^ y)
         .collect::<Vec<u8>>();
 }
 
-fn crack_single_byte_xor(input: &Vec<u8>) -> (u8, Vec<u8>)
-{
+fn crack_single_byte_xor(input: &Vec<u8>) -> (u8, Vec<u8>) {
     let characters = (0..=255).collect::<Vec<u8>>();
-    let mut strings: Vec<(u8, Vec<u8>)> = characters.iter()
+    let mut strings: Vec<(u8, Vec<u8>)> = characters
+        .iter()
         .map(|character| {
             let mask = vec![character.clone(); input.len()];
             return (character.clone(), fixed_xor(input, &mask));
@@ -54,23 +55,24 @@ fn crack_single_byte_xor(input: &Vec<u8>) -> (u8, Vec<u8>)
     return strings.first().unwrap().clone();
 }
 
-fn crack_repeating_xor(input: &Vec<u8>) -> (Vec<u8>, Vec<u8>)
-{
-    let key_size = (2..=40).fold((0, 0.0), |size, k| {
-        let chunks = input.chunks_exact(k * 2);
-        let total_chunks = chunks.len() as f32;
-        let distance = chunks.fold(0.0, |d, chunk| d + hamming_distance(&chunk[0..k], &chunk[k..2 * k]) as f32 / k as f32) / total_chunks;
+fn crack_repeating_xor(input: &Vec<u8>) -> (Vec<u8>, Vec<u8>) {
+    let key_size = (2..=40)
+        .fold((0, 0.0), |size, k| {
+            let chunks = input.chunks_exact(k * 2);
+            let total_chunks = chunks.len() as f32;
+            let distance = chunks.fold(0.0, |d, chunk| {
+                d + hamming_distance(&chunk[0..k], &chunk[k..2 * k]) as f32 / k as f32
+            }) / total_chunks;
 
-        if size.1 == 0.0 || distance < size.1 {
-            (k, distance)
-        } else {
-            size
-        }
-    }).0;
+            if size.1 == 0.0 || distance < size.1 {
+                (k, distance)
+            } else {
+                size
+            }
+        })
+        .0;
 
-    let blocks: Vec<Vec<u8>> = input.chunks(key_size)
-        .map(|x| Vec::from(x))
-        .collect();
+    let blocks: Vec<Vec<u8>> = input.chunks(key_size).map(|x| Vec::from(x)).collect();
 
     let mut transposed_blocks: Vec<Vec<u8>> = Vec::with_capacity(blocks.len());
     for i in 0..key_size {
@@ -80,7 +82,10 @@ fn crack_repeating_xor(input: &Vec<u8>) -> (Vec<u8>, Vec<u8>)
     for position in 0..key_size {
         for block in &blocks {
             match block.get(position) {
-                Some(byte) => transposed_blocks.get_mut(position).unwrap().push(byte.clone()),
+                Some(byte) => transposed_blocks
+                    .get_mut(position)
+                    .unwrap()
+                    .push(byte.clone()),
                 None => {}
             }
         }
@@ -107,8 +112,7 @@ fn crack_repeating_xor(input: &Vec<u8>) -> (Vec<u8>, Vec<u8>)
     return (key, final_bytes);
 }
 
-fn sort_by_score_desc(left: &Vec<u8>, right: &Vec<u8>) -> Ordering
-{
+fn sort_by_score_desc(left: &Vec<u8>, right: &Vec<u8>) -> Ordering {
     let left_score = english_score(left);
     let right_score = english_score(right);
 
@@ -118,11 +122,10 @@ fn sort_by_score_desc(left: &Vec<u8>, right: &Vec<u8>) -> Ordering
         Ordering::Greater
     } else {
         Ordering::Equal
-    }
+    };
 }
 
-fn english_score(input: &Vec<u8>) -> i64
-{
+fn english_score(input: &Vec<u8>) -> i64 {
     let mut score = 0;
 
     for byte in input {
@@ -130,7 +133,7 @@ fn english_score(input: &Vec<u8>) -> i64
             score -= 1000;
         }
 
-        if (*byte >=65 && *byte <=90) || (*byte >= 97 && *byte <= 122) {
+        if (*byte >= 65 && *byte <= 90) || (*byte >= 97 && *byte <= 122) {
             score += 1;
         }
     }
@@ -138,8 +141,7 @@ fn english_score(input: &Vec<u8>) -> i64
     return score;
 }
 
-fn hamming_distance(left: &[u8], right: &[u8]) -> u32
-{
+fn hamming_distance(left: &[u8], right: &[u8]) -> u32 {
     let mut left_bits = BitVec::from_bytes(left);
     let mut right_bits = BitVec::from_bytes(right);
     let left_len = left_bits.len();
@@ -151,22 +153,21 @@ fn hamming_distance(left: &[u8], right: &[u8]) -> u32
         left_bits.grow(right_len - left_len, false);
     }
 
-    return left_bits.iter()
+    return left_bits
+        .iter()
         .zip(right_bits.iter())
         .fold(0, |sum, (l, r)| sum + if l == r { 0 } else { 1 });
 }
 
 #[cfg(test)]
-mod test
-{
-    use std::fs::File;
-    use std::io::BufReader;
-    use std::io::BufRead;
+mod test {
     use super::*;
+    use std::fs::File;
+    use std::io::BufRead;
+    use std::io::BufReader;
 
     #[test]
-    fn case1test()
-    {
+    fn case1test() {
         let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
         let expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
 
@@ -174,44 +175,51 @@ mod test
     }
 
     #[test]
-    fn case2test()
-    {
+    fn case2test() {
         let input = "1c0111001f010100061a024b53535009181c";
         let xor = "686974207468652062756c6c277320657965";
         let expected = "746865206b696420646f6e277420706c6179";
 
-        assert_eq!(expected, hex_encode(&fixed_xor(&decode(input).unwrap(), &decode(xor).unwrap())));
+        assert_eq!(
+            expected,
+            hex_encode(&fixed_xor(&decode(input).unwrap(), &decode(xor).unwrap()))
+        );
     }
 
     #[test]
-    fn case3test()
-    {
+    fn case3test() {
         let input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
         let expected = "Cooking MC's like a pound of bacon";
 
-        assert_eq!(expected, String::from_utf8(crack_single_byte_xor(&decode(input).unwrap()).1).unwrap());
+        assert_eq!(
+            expected,
+            String::from_utf8(crack_single_byte_xor(&decode(input).unwrap()).1).unwrap()
+        );
     }
 
     #[test]
-    fn case4test()
-    {
+    fn case4test() {
         let file = BufReader::new(File::open("4.txt").unwrap());
-        let mut lines: Vec<(u8, Vec<u8>)> = file.lines()
+        let mut lines: Vec<(u8, Vec<u8>)> = file
+            .lines()
             .filter_map(|x| x.ok())
             .map(|x| decode(x).unwrap())
             .map(|x| crack_single_byte_xor(&x))
             .collect();
 
         lines.sort_unstable_by(|left, right| sort_by_score_desc(&left.1, &right.1));
-        assert_eq!("Now that the party is jumping\n", String::from_utf8(lines.first().unwrap().clone().1).unwrap());
+        assert_eq!(
+            "Now that the party is jumping\n",
+            String::from_utf8(lines.first().unwrap().clone().1).unwrap()
+        );
     }
 
     #[test]
-    fn case5test()
-    {
+    fn case5test() {
         let key: Vec<u8> = "ICE".bytes().collect();
         let bytes: Vec<u8> = "Burning 'em, if you ain't quick and nimble
-I go crazy when I hear a cymbal".bytes()
+I go crazy when I hear a cymbal"
+            .bytes()
             .collect();
         let xord = repeating_xor(&bytes, &key);
 
@@ -219,8 +227,7 @@ I go crazy when I hear a cymbal".bytes()
     }
 
     #[test]
-    fn hamming_test()
-    {
+    fn hamming_test() {
         let left: Vec<u8> = "this is a test".bytes().collect();
         let right: Vec<u8> = "wokka wokka!!!".bytes().collect();
         assert_eq!(37, hamming_distance(&left, &right));
